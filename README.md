36d775c96b71e08cdcb1d934c3da7f2c7a981ba3
Akash Gandhawa <wagandhawarodrigo@gmail.com>
2026-03-21 12:57:02 +0530

    Formatted the code to be more readable and added some minor fixes

--- README.md
+++ README.md
@@ -9,42 +9,35 @@ Simple program that models a system of an textile manufacturing company to bette
 ## System Overview:
 ### 1. Receiving Dock
 * Data Structure: **Queue**
-* Main Methods: IsEmpty, Enqueue, Dequeue, Display
 * Data Elements: `{truck_id, priority, supplier, material_type, date}`
 * Special Task: Sorted by `priority`
 
 ### 2. Main Warehouse
 * Data Structure: **Doubly Linked List**
-* Main Methods: Add, Delete, Update
 * Data Elements: `{slot_no, material_type, stock_kg}`
 * Special Task: *add task*
 
 ### 3. Secondary Stock
 * Data Structure: **Doubly Linked List**
-* Main Methods: Add, Delete, Update
 * Data Elements: `{material_type, stock_kg}`
 * Special Task: *add task*
 
 ### 4. Production Floor
 * Data Structure: **Array**
-* Main Methods: Add, Delete, Update
 * Data Elements: `{machine_id, status, maintainance_req}`
 * Special Task: *add task*
 
 ### 5. Sewing Operator
 * Data Structure: **Linked List**
-* Main Methods: Add, Delete, Update
 * Data Elements: `{operator_id, machine_id, piece_type, pieces_completed, salary}`
 * Special Task: Sorted by `pieces_completed`
 
 ### 6. Order Dispatch
 * Data Structure: **Linked List**
-* Main Methods: Add, Delete, Update
 * Data Elements: `{order_id, receiver, priority, item, item_count, price}`
 * Special Task: Sorted by `priority`
 
 ### 7. Security
-* Data Structure: Array
-* Main Methods: Add, Delete, Update
+* Data Structure: **Array**
 * Data Elements: `{guard_id, is_active, patrol_point, shift}`
 * Special Task: *add task*
--- order_dispatch.c
+++ order_dispatch.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "factory.h"
 
-
 // typedef struct Order{
 //     int order_id;
 //     int receiver_id;
@@ -13,131 +12,141 @@
 // struct Order* next;
 // }Order;
 
-struct Order* orderhead=NULL;
+struct Order *orderhead = NULL;
 
 // Function to create a new order with the given order details
-void AddOrder(int order_id,int receiver_id,int priority,char item_type,int item_count,float price){
-       Order* newOrder =(struct Order*)malloc(sizeof(struct Order));
-       if(newOrder==NULL){
+void AddOrder(int order_id, int receiver_id, int priority, char item_type, int item_count, float price)
+{
+    Order *newOrder = (struct Order *)malloc(sizeof(struct Order));
+    if (newOrder == NULL)
+    {
         printf("Memory Allocation Failed");
-       }
-       newOrder->order_id=order_id;
-       newOrder->receiver_id=receiver_id;
-       newOrder->priority=priority;
-       newOrder->item_type=item_type;
-       newOrder->item_count=item_count;
-       newOrder->price=price;
+    }
+    newOrder->order_id = order_id;
+    newOrder->receiver_id = receiver_id;
+    newOrder->priority = priority;
+    newOrder->item_type = item_type;
+    newOrder->item_count = item_count;
+    newOrder->price = price;
 
-       newOrder->next=orderhead;
-       orderhead=newOrder;
-       printf("New Order Added Successfully!\n");
-   };
+    newOrder->next = orderhead;
+    orderhead = newOrder;
+    printf("New Order Added Successfully!\n");
+};
 //    -----------------------------------------------------
 
-
 // delete an order from the linked list based on the order_id
-void DeleteOrder(int order_id){
-    if(orderhead==NULL){
+void DeleteOrder(int order_id)
+{
+    if (orderhead == NULL)
+    {
         printf("No orders to delete\n");
     }
-    if(orderhead->order_id==order_id){
-        Order* temp=orderhead;
-        orderhead=orderhead->next;
+    if (orderhead->order_id == order_id)
+    {
+        Order *temp = orderhead;
+        orderhead = orderhead->next;
         free(temp);
         printf("Order deleted successfully from the first position\n");
         return;
     }
-    Order* current=orderhead->next;
-    Order* previous=orderhead;
+    Order *current = orderhead->next;
+    Order *previous = orderhead;
 
-    while(current!=NULL){
-        if (current->order_id==order_id){
-            previous->next=current->next;
+    while (current != NULL)
+    {
+        if (current->order_id == order_id)
+        {
+            previous->next = current->next;
             free(current);
             printf("Order deleted successfully from the middle position\n");
             return;
         }
-        previous=current;
-        current=current->next;
+        previous = current;
+        current = current->next;
     }
     printf("Order not found\n");
 };
 // -----------------------------------------------------
 
 // update an order in the linked list based on the order_id
-void UpdateOrder(int order_id,int receiver_id,int priority,char item_type,int item_count,float price){
-    Order* current=orderhead;
-    while(current!=NULL){
-        if(current->order_id==order_id){
-            current->receiver_id=receiver_id;
-            current->priority=priority;
-            current->item_type=item_type;
-            current->item_count=item_count;
-            current->price=price;
+void UpdateOrder(int order_id, int receiver_id, int priority, char item_type, int item_count, float price)
+{
+    Order *current = orderhead;
+    while (current != NULL)
+    {
+        if (current->order_id == order_id)
+        {
+            current->receiver_id = receiver_id;
+            current->priority = priority;
+            current->item_type = item_type;
+            current->item_count = item_count;
+            current->price = price;
             printf("Order updated successfully\n");
             return;
         }
-        current=current->next;
+        current = current->next;
     }
     printf("Order not found\n");
 };
 // ------------------------------------------------------
 
 // display all the orders in the linked list
-void DisplayOrders(){
-    struct Order* current=orderhead;
-    if(current==NULL){
+void DisplayOrders()
+{
+    struct Order *current = orderhead;
+    if (current == NULL)
+    {
         printf("No orders to display\n");
         return;
     }
-    while(current!=NULL){
+    while (current != NULL)
+    {
         printf("Order ID: %d, Receiver ID: %d, Priority: %d, Item Type: %c, Item Count: %d, Price: %.2f\n",
-               current->order_id,current->receiver_id,current->priority,current->item_type,current->item_count,current->price);
-        current=current->next;
+               current->order_id, current->receiver_id, current->priority, current->item_type, current->item_count, current->price);
+        current = current->next;
     }
 };
 // ------------------------------------------------------
 
-void SortByPriority(){
-    if(orderhead==NULL){
+void SortByPriority()
+{
+    if (orderhead == NULL)
+    {
         printf("No Orders to sort\n");
         return;
     }
-    Order* curr=orderhead;
-    while(curr!=NULL){
-        Order* next=curr->next;
-        Order* min=curr;
+    Order *curr = orderhead;
+    while (curr != NULL)
+    {
+        Order *next = curr->next;
+        Order *min = curr;
 
-        while(next!=NULL){
-            if(min->priority>next->priority){
-                min=next;
+        while (next != NULL)
+        {
+            if (min->priority > next->priority)
+            {
+                min = next;
             }
-            next=next->next;
+            next = next->next;
         }
 
-        Order temp=*curr;
+        Order temp = *curr;
 
-        curr->order_id=min->order_id;
-        curr->receiver_id=min->receiver_id;
-        curr->priority=min->priority;
-        curr->item_type=min->item_type;
-        curr->item_count=min->item_count;
-        curr->price=min->price;
+        curr->order_id = min->order_id;
+        curr->receiver_id = min->receiver_id;
+        curr->priority = min->priority;
+        curr->item_type = min->item_type;
+        curr->item_count = min->item_count;
+        curr->price = min->price;
 
-        min->order_id=temp.order_id;
-        min->receiver_id=temp.receiver_id;
-        min->priority=temp.priority;
-        min->item_type=temp.item_type;
-        min->item_count=temp.item_count;
-        min->price=temp.price;
+        min->order_id = temp.order_id;
+        min->receiver_id = temp.receiver_id;
+        min->priority = temp.priority;
+        min->item_type = temp.item_type;
+        min->item_count = temp.item_count;
+        min->price = temp.price;
 
-
-        curr=curr->next;
-
+        curr = curr->next;
     }
 };
-
-
-
-
-
